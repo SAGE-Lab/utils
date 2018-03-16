83a1bdcb81cac6c18799cebb38797c5db422dd12
Simone Vuotto <simov8@gmail.com>
2018-03-16 17:19:45 +0100

    Add comment

--- src/main/java/sage/collections/Trie.java
+++ src/main/java/sage/collections/Trie.java
@@ -3,6 +3,13 @@ package sage.collections;
 import java.util.Iterator;
 import java.util.List;
 
+/**
+ * Implementation of a Trie data structure, not limited to character sequences.
+ * This class can handle sequences of arbitrary elements K and retrieve them with an iterator.
+ *
+ * @param <K> The type of key used in the Trie
+ * @author Simone Vuotto
+ */
 public class Trie <K> implements Iterable<List<K>> {
 
     TrieNode<K> root;
@@ -11,6 +18,10 @@ public class Trie <K> implements Iterable<List<K>> {
         root = new TrieNode<>(null, false);
     }
 
+    /**
+     * Insert a sequence of elements K in the trie.
+     * @param path the list of elements to insert
+     */
     public void insert(List<K> path) {
 
         TrieNode<K> t = root;
